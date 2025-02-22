3a71c21f22ef7eb199c2f8d00a34b431ae6d48af
B34rly <bearz4lifez@gmail.com>
2025-02-22 18:17:50 +1100

    completed chaos backend q1

--- chaos/src/data.rs
+++ chaos/src/data.rs
@@ -1,22 +1,41 @@
-use axum::{http::StatusCode, response::IntoResponse, Json};
+use axum::{http::{request, StatusCode}, response::IntoResponse, Json};
 use serde::{Deserialize, Serialize};
 
 pub async fn process_data(Json(request): Json<DataRequest>) -> impl IntoResponse {
     // Calculate sums and return response
+    let mut int_count: i32 = 0;
+    let mut string_length_count: i32 = 0;
+
+    for some_value in &request.data
+    {
+        match some_value
+        {
+            StringOrInt::Num(number) => int_count += number,
+            StringOrInt::Message(message) => string_length_count += message.chars().count() as i32
+        }
+    }
 
     let response = DataResponse {
-        
+        string_len: string_length_count, int_sum: int_count
     };
-
     (StatusCode::OK, Json(response))
 }
 
 #[derive(Deserialize)]
-pub struct DataRequest {
-    // Add any fields here
+#[serde(untagged)]
+pub enum StringOrInt{
+    Message(String),
+    Num(i32)
 }
+
+#[derive(Deserialize)]
+pub struct DataRequest {
+    data: Vec<StringOrInt>
+}
+
 
 #[derive(Serialize)]
 pub struct DataResponse {
-    // Add any fields here
+    string_len: i32,
+    int_sum: i32
 }
